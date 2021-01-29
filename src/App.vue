aa608b8a80b2b75a749f928080ddc94fba08243a
uyamazak <yu.yamazaki85@gmail.com>
2021-01-29 18:34:22 +0900

    vite

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <ToriFront />
+  <ToriSide />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import ToriFront from './components/ToriFront.vue'
+import ToriSide from './components/ToriSide.vue'
+
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    ToriFront,
+    ToriSide,
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
